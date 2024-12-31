02e5edf64f663212e574d80cb13da2a5cf15e084
HarshPatel2010 <harshpatel29301@gmail.com>
2024-12-31 11:12:10 +0530

    serde added for JSON

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use serde::{ Serialize,Deserialize};
+#[derive(Seralize,Deserialize)]
+struct Paragraph{
+    name:String
+}
+struct Article{
+    article:String,
+    author:String,
+    paragraph:Vec<Paragraph>,
+}
 fn main() {
+
     println!("Hello, world!");
 }
