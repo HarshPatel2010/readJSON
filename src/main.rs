e49166a56e43121acf88633517d1754f95fe09f6
HarshPatel2010 <harshpatel29301@gmail.com>
2024-12-31 13:00:40 +0530

    writing json completed

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ struct Article {
 #[derive(Serialize, Deserialize, Debug)]
 struct Para{
     name:String,
-};
+}
 #[derive(Serialize, Deserialize, Debug)]
 struct Art{
 art:String,
@@ -39,8 +39,13 @@ fn main() {
 
     // write JSON
     let art = Art{
-        art:String::from("how to work with json")
-    }
+        art:String::from("how to work with json"),
+        auth:String::from("i am auth from the string okay!"),
+        Para:vec![Para{name: String::from("starting sentencex")},Para{name: String::from("body paragraph")},Para{name: String::from("end of the paragraph")},Para{name: String::from("paragraph")},Para{name: String::from("the end of the paragraph")}]
+    };
+
+    let wjson = serde_json::to_string(&art).unwrap();
+    println!("{}", wjson);
 
 }
 fn read_json_type(json:&str) -> Article {
