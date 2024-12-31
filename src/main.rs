6d3578a854481f6c4f5e09abfd7a7d58554bcab3
HarshPatel2010 <harshpatel29301@gmail.com>
2024-12-31 11:59:41 +0530

    completed

--- src/main.rs
+++ src/main.rs
@@ -12,20 +12,21 @@ struct Article {
 }
 fn main() {
     let json = r#"
-   {
-   "article": {},
-   "author": {},
-   "paragraph": [
-   {"name":"starting sentence"},
-   {"name":"body paragraph"},
-   {"name":"end of the paragraph"},
-   ]}"#;
+    {
+        "article": "An Example Article",
+        "author": "John Doe",
+        "paragraph": [
+            {"name":"starting sentencex"},
+            {"name":"body paragraph"},
+            {"name":"end of the paragraph"}
+        ]
+    }"#;
 
     let parsed:Article = read_json_type(json);
-    println!("the first sentece of para is : {}",parsed.paragraph[0].name);
+    println!("the first sentece of para is : {}",parsed.article);
 
 }
-fn read_json_type(json:&str)->Article {
+fn read_json_type(json:&str)->Article{
     let parsed:Article = serde_json::from_str(json).unwrap();
     return parsed
 }
