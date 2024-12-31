a164b439ed899c315257475a89afdea1a2beac2f
HarshPatel2010 <harshpatel29301@gmail.com>
2024-12-31 11:47:47 +0530

    parsed func has made

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,31 @@
-use serde::{ Serialize,Deserialize};
-#[derive(Seralize,Deserialize)]
-struct Paragraph{
-    name:String
+use serde::{Serialize, Deserialize};
+use serde_json;
+#[derive(Serialize, Deserialize, Debug)]
+struct Paragraph {
+    name: String,
 }
-struct Article{
-    article:String,
-    author:String,
-    paragraph:Vec<Paragraph>,
+#[derive(Serialize, Deserialize, Debug)]
+struct Article {
+    article: String,
+    author: String,
+    paragraph: Vec<Paragraph>,
 }
 fn main() {
+    let json = r#"
+   {
+   "article": {},
+   "author": {},
+   "paragraph": [
+   {"name":"starting sentence"},
+   {"name":"body paragraph"},
+   {"name":"end of the paragraph"},
+   ]}"#;
 
-    println!("Hello, world!");
+    let parsed:Article = read_json_type(json);
+    println!("the first sentece of para is : {}",parsed.paragraph[0].name);
+
+}
+fn read_json_type(json:&str)->Article {
+    let parsed:Article = serde_json::from_str(json).unwrap();
+    return parsed
 }
